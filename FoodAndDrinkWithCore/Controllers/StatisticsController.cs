8acac65f8fbc0f5faddf0b981d4017521bffd757
iamteamstar <azadkocak29@gmail.com>
2026-01-29 04:01:36 +0300

    Statistic queries ended

--- FoodAndDrinkWithCore/Controllers/StatisticsController.cs
+++ FoodAndDrinkWithCore/Controllers/StatisticsController.cs
@@ -20,7 +20,6 @@ namespace FoodAndDrinkWithCore.Controllers
 			ViewBag.d2 = totC;
 
 			var foid = c.categories.Where(x => x.CategoryName == "Fruit").Select(y => y.CategoryID).FirstOrDefault();
-			ViewBag.d = foid;
 			var totFruit = c.foods.Where(x => x.CategoryID == foid).Count();
 			ViewBag.d3 = totFruit;
 
@@ -38,6 +37,26 @@ namespace FoodAndDrinkWithCore.Controllers
 
 			var sumFoodStock = c.foods.Max(x=>x.FoodStock);
 			ViewBag.d8 = sumFoodStock;
+
+			var maxStockFood = c.foods.OrderByDescending(x => x.FoodStock).Select(y => y.FoodName).FirstOrDefault();
+			ViewBag.d9 = maxStockFood;
+
+			var minStockFood = c.foods.OrderBy(x => x.FoodStock).Select(y => y.FoodName).FirstOrDefault();//orderby oto olarak azları verir yani a dan z ye sıralar küçükten büyüğe 
+			ViewBag.d10 = minStockFood;
+
+			var foodAvg = c.foods.Average(x => x.FoodPrice).ToString("0.000");//tostring basamak sayısı
+			ViewBag.d11 = foodAvg;
+
+			var dgr = c.categories.Where(x => x.CategoryName == "Fruit").Select(y => y.CategoryID).FirstOrDefault();
+			var dgrp = c.foods.Where(y => y.CategoryID == dgr).Sum(x => x.FoodStock);
+			ViewBag.d12 = dgrp;
+
+			var dgr1 = c.categories.Where(x => x.CategoryName == "Vegetables").Select(y => y.CategoryID).FirstOrDefault();
+			var dgr1p = c.foods.Where(y => y.CategoryID == dgr1).Sum(x => x.FoodStock);
+			ViewBag.d13 = dgr1p;
+
+			var dgr2= c.foods.OrderByDescending(x => x.FoodPrice).Select(y => y.FoodName).FirstOrDefault();
+			ViewBag.d14 = dgr2;
 
 			return View();
 		}
--- FoodAndDrinkWithCore/obj/Debug/net8.0/FoodAndDrinkWithCore.AssemblyInfo.cs
+++ FoodAndDrinkWithCore/obj/Debug/net8.0/FoodAndDrinkWithCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodAndDrinkWithCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b9a9a86a754b32743426e066fcacc8aba122d71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2abd11e78224f8d1943744e77d62951dc693afee")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodAndDrinkWithCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodAndDrinkWithCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
